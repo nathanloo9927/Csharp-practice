a02c5059e7a1954b387ee8d2f0a47623e0731b51
Nathan Loo <nloo2@uic.edu>
2021-07-09 15:35:43 -0500

    4a complete

---
+++ followalong4/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using followalong4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace followalong4.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+        public CustomerController()
+        {
+            customers.Add(new CustomerModel { Id = 0, Name = "Milk Bear", Age = 20 });
+            customers.Add(new CustomerModel { Id = 1, Name = "Mocha Bear", Age = 22 });
+            customers.Add(new CustomerModel { Id = 2, Name = "Tenko", Age = 17 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Himiko", Age = 17 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Maki", Age = 17 });
+            customers.Add(new CustomerModel { Id = 5, Name = "Shuichi", Age = 17 });
+            customers.Add(new CustomerModel { Id = 6, Name = "Kaito", Age = 17 });
+            customers.Add(new CustomerModel { Id = 7, Name = "K1-B0", Age = 37 });
+        }
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public IActionResult ShowOnePerson(int id)
+        {
+            CustomerModel c = customers.FirstOrDefault(x => x.Id == id);
+            return PartialView(c);
+        }
+    }
+}
---
+++ followalong4/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    console.log("Degenerate Male");
+
+    $(".customerRadio").change(function () {
+        console.log("diabolical");
+        updateCustomer();
+    });
+
+    function updateCustomer() {
+        $.ajax({
+            type: "POST",
+            url: 'Customer/ShowOnePerson',
+            data: $("form").serialize(),
+            success: function (data) {
+                console.log(data);
+                $("#customerInformationArea").html(data);
+            }
+        });
+    }
+
+    $("#selectCustomer").click(function () {
+        event.preventDefault();
+        console.log("nyehhhhh");
+        updateCustomer();
+    });
+});
